69849c433243d84758d63eaae79506485c5f7fa5
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-24 14:01:44 -0400

    Removed unused configuration variables

--- reIndexer/cfg.py
+++ reIndexer/cfg.py
@@ -8,10 +8,8 @@ class config:
     backtest_start = pd.to_datetime('2015-01-01').tz_localize(tz_local)
     backtest_end = pd.to_datetime('2018-03-01').tz_localize(tz_local)
 
-    # Computation windows
-    port_rebalancing_period = 21  # days
+    # Backtest frequency configuration
     backtest_frequency = 'daily'  # Must be either 'daily' or 'minute'
-
 
     # Portfolio configuration
     capital_base = 1e10
@@ -20,11 +18,9 @@ class config:
     # Sector universe (set at backtest initialization)
     sector_universe = None
 
-    # Synthetic ETF
-    setf_base_value = 125.0  # Identical to NASDAQ base value
+    # Synthetic ETF`
     setf_lookback_window = 252  # days (1 work year)
     setf_data_frequency = '1d'  # '1m' or '1d' for minute/daily respectively
-    setf_restructure_window = setf_lookback_window / 12  # days (12 times / year)
 
     # Restructuring and rebalancing triggers
     # NOTE: The 'week' key indicates the week of the rebalance; counting here
