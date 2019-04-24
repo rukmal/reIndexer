593d606f056f959c20056cf0e0efcd1feb8b840c
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-24 15:17:11 -0400

    Added docstring for minvar portfolio

--- reIndexer/portfolio/minvar.py
+++ reIndexer/portfolio/minvar.py
@@ -6,8 +6,15 @@ import numpy as np
 
 
 class MinimumVariance():
+    """Class encapsulating functionality to compute a minimum variance
+    portfolio, and related functionality.
+    """
 
     def __init__(self):
+        """Initialization method for `MinimumVariance`. Sets initial
+        optimization constraints, and bounds to ensure that no short sales
+        are possible.
+        """
         # See: http://bit.ly/2IzJWE0 and http://bit.ly/2IPlTQP for more on this
         # NOTE: Equality constraints are tested to be equal to zero, so the sum
         #       of the weights - 1 must be 0
@@ -22,11 +29,27 @@ class MinimumVariance():
             }
         )
 
-        # Bounds do not allow flr
+        # Bounds for no shorts
         self.bounds_base = ((0, 1),)
 
     def computeWeights(self, log_rets: np.ndarray, prev_weights: np.array=None)\
         -> np.array:
+        """Function to compute portfolio weights, given a matrix of log-returns
+        for a set of assets. This function uses scipy's optimize.minimize
+        function to return the weights of the assets in a minimum variance
+        portfolio with no short sales allowed.
+        
+        Arguments:
+            log_rets {np.ndarray} -- Matrix of log returns of the assets.
+        
+        Keyword Arguments:
+            prev_weights {np.array} -- Previous iteration weights of the minimum
+                                       variance portfolio (default: {None}).
+        
+        Returns:
+            np.array -- Vector of asset weights.
+        """
+
         # Computing covariance matrix
         cov_mat = np.cov(log_rets) * config.setf_lookback_window
 
