a352b7b0f613827b5f3c9df162004bd87402e085
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-24 16:09:05 -0400

    Finished backtest engine documentation

--- reIndexer/backtest/zipline_backtest.py
+++ reIndexer/backtest/zipline_backtest.py
@@ -6,8 +6,7 @@ from ..synthetic_etf import PriceWeightedETF
 
 from zipline import run_algorithm
 from zipline.algorithm import TradingAlgorithm
-from zipline.api import order_target_percent, record, set_long_only,\
-    set_max_leverage, symbol
+from zipline.api import order_target_percent, record, set_long_only, symbol
 from zipline.data.bar_reader import NoDataForSid
 from zipline.finance.execution import MarketOrder
 from zipline.errors import SymbolNotFound
@@ -18,7 +17,24 @@ import pandas as pd
 
 
 class Backtest():
+    """Module to handle and run the Zipline backtesting framework.
+
+    This module is the main driver for reIndexer. It orchestrates synthetic ETF
+    creation, portfolio rebalancing, zipline logging, and sector universe
+    validation.
+
+    Backtest initializes with the target simulation sector universe, and reads
+    all other configuration information from the reIndexer configuration file.
+    """
+
     def __init__(self, sector_universe: Universe):
+        """Initialization method for the Backtest module. Binds the target
+        sector universe to an instance variable.
+        
+        Arguments:
+            sector_universe {Universe} -- Target simulation sector universe.
+        """
+
         # Binding sector universe to class variable
         logging.debug('Successfully loaded sector universe {0}'
             .format(sector_universe.getUniverseName()))
@@ -27,6 +43,9 @@ class Backtest():
     @staticmethod
     def zipline_initialize(context: TradingAlgorithm):
         """Zipline backtest initialization method override.
+
+        Initializes context namespace variables used during the portfolio
+        optimization, and sets zipline configuration options for the simulation.
         
         Arguments:
             context {TradingAlgorithm} -- Context variable for the algorithm
@@ -41,14 +60,27 @@ class Backtest():
         # Enforcing long trades only
         set_long_only()
 
-        # Setting maximum leverage to 1 (i.e. no leverage)
-        set_max_leverage(max_leverage=1.0)
-
         # Initializing utilities module
         context.util = Utilities()
 
     @staticmethod
     def zipline_handle_data(context: TradingAlgorithm, data: BarData):
+        """Zipline `handle_data` method override. Handles all trading
+        operations for the algorithm. Additionally, this method also handles
+        synthetic ETF initialization, portfolio balancing, and logging.
+
+        In summary, this method initializes all synthetic ETFs on the first
+        iteration, balances, and opens positions in the necessary assets. After
+        that, it checks the current date of the simulation against the triggers
+        for portfolio balancing and ETF restructuring provided in the
+        configuration file to initialize a rebalancing or restructuring
+        operation, respectively.
+        
+        Arguments:
+            context {TradingAlgorithm} -- Zipline context namespace variable.
+            data {BarData} -- Instance zipline data bundle.
+        """
+
         # First run operations
         if (context.first_run):
             # Validate sector universe
@@ -96,6 +128,15 @@ class Backtest():
 
     @staticmethod
     def buildSyntheticETFs(context: TradingAlgorithm, zipline_data: BarData):
+        """Function to build synthetic ETFs. This is the initialization method
+        for the ETFs, and is meant to be called during the first iteration of
+        the simulation only.
+        
+        Arguments:
+            context {TradingAlgorithm} -- Zipline context namespace variable.
+            zipline_data {BarData} -- Instance zipline data bundle.
+        """
+
         # Looping through each sector
         for sector_label in config.sector_universe.getSectorLabels():
             # Initializing synthetic ETF, storing in dictionary
@@ -110,6 +151,25 @@ class Backtest():
     @staticmethod
     def rebalancePortfolio(context: TradingAlgorithm, zipline_data: BarData,
         update_positions: bool=True) -> np.array:
+        """Function to rebalance a portfolio of synthetic ETFs, with the option
+        to trigger an execution of trades within Zipline to enforce the new
+        portfolio synthetic ETF asset allocations.
+
+        This method also handles logging, and binds the new portfolio weights
+        to the `context.port_w`, and `context.port_weights` variables.
+        
+        Arguments:
+            context {TradingAlgorithm} -- Zipline context namespace variable.
+            zipline_data {BarData} -- Instance zipline data bundle.
+        
+        Keyword Arguments:
+            update_positions {bool} -- Flag to update positions in Zipline
+                                       (default: {True}).
+        
+        Returns:
+            np.array -- Portfolio weights.
+        """
+
         # Updating parameters for each of the sectors
         [context.synthetics[i].updateParameters(zipline_data=zipline_data)
             for i in config.sector_universe.getSectorLabels()]
@@ -139,6 +199,22 @@ class Backtest():
     @staticmethod
     def restructureETF(context: TradingAlgorithm, zipline_data: BarData,
         update_positions: bool=True):
+        """Function to restructure the ETFs. This function calls the internal
+        synthetic ETF restructuring method to update weights within each of the
+        synthetic ETF objects.
+
+        This function also handles logging, and provides the option to make
+        trades to make trades updating asset positions in zipline.
+        
+        Arguments:
+            context {TradingAlgorithm} -- Zipline context namespace variable.
+            zipline_data {BarData} -- Instance zipline data bundle.
+        
+        Keyword Arguments:
+            update_positions {bool} -- Flag to update positions in Zipline
+                                       (default: {True}).
+        """
+
         # Updating weights for each of the synthetic ETF components
         [context.synthetics[i].updateWeights(zipline_data=zipline_data)
             for i in config.sector_universe.getSectorLabels()]
@@ -149,6 +225,19 @@ class Backtest():
 
     @staticmethod
     def updatePositions(context: TradingAlgorithm):
+        """Function to update asset positions in Zipline.
+
+        This function computes specific asset proportions in the final portfolio
+        by multiplying the weight of the sector containing the asset by the
+        weight of the asset within the sector. Then, it uses zipline's
+        `order_target_percent` function to specify the percentage (specified
+        as a decimal) of the position for a given asset in the portofolio as a
+        whole.
+        
+        Arguments:
+            context {TradingAlgorithm} -- Zipline context namespace variable.
+        """
+
         # Looping through each sector
         for sector_label in config.sector_universe.getSectorLabels():
             # Isolating current sector synthetic ETF
@@ -169,17 +258,6 @@ class Backtest():
                 )
                 logging.debug('Updated portfolio ticker {0} weight to {1}%'.
                     format(ticker, port_ticker_weight * 100))
-
-    def run(self) -> pd.DataFrame:
-        return run_algorithm(
-            start=config.backtest_start,
-            end=config.backtest_end,
-            capital_base=config.capital_base,
-            initialize=self.zipline_initialize,
-            handle_data=self.zipline_handle_data,
-            data_frequency=config.backtest_frequency,
-            bundle='quandl'
-        )
 
     @staticmethod
     def validateSectorUniverse(candidate_sector_universe: Universe, data):
@@ -208,3 +286,21 @@ class Backtest():
         
         # Return 'clean' sector universe
         return candidate_sector_universe
+
+    def run(self) -> pd.DataFrame:
+        """Function to run the simulation; triggers zipline's `run_algorithm`,
+        with appropriate function overrides.
+        
+        Returns:
+            pd.DataFrame -- Zipline simulation results.
+        """
+
+        return run_algorithm(
+            start=config.backtest_start,
+            end=config.backtest_end,
+            capital_base=config.capital_base,
+            initialize=self.zipline_initialize,
+            handle_data=self.zipline_handle_data,
+            data_frequency=config.backtest_frequency,
+            bundle='quandl'
+        )
