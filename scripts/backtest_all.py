514e6424384927da870ded8a65a58ccecc9c2dfd
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-04-28 21:39:35 -0400

    Added script to backtest all candidate sectors

---
+++ scripts/backtest_all.py
@@ -0,0 +1,67 @@
+# Script to run a complete backtest on all candidate sector universes in the
+# `sector_universes/learned_sector_candidates`
+# This script isolates a list of common denominator companies (that are common
+# across all candidate sectorization schemes, and only uses these)
+
+import logging
+import os
+import pandas as pd
+
+from context import reIndexer
+
+
+# Setting logging output to a file, setting level
+logging.basicConfig(filename='tmp/out.log', level=logging.INFO)
+
+# Folder containing candidate sector universe files
+sector_folder = 'sector_universes/learned_sector_candidates/'
+# Output folder for pickled backtest results dataframes
+pickle_out_folder = 'sector_universes/learned_sectors/pickle/'
+# Output folder for excel backtest result files
+excel_out_folder = 'sector_universes/learned_sectors/excel/'
+
+
+def backtestAll():
+    candidate_files = [f for f in os.listdir(sector_folder)
+        if f.endswith('.csv')]
+    print('Found {0} candidate sector files'.format(len(candidate_files)))
+
+    counter = 0
+
+    for f in candidate_files:
+        print('Currently backtesting {0}'.format(f))
+
+        # Isolating sector universe name
+        universe_name, _ = f.split('.')
+
+        # Creating reIndexer sector from file
+        candidate_universe = reIndexer.Universe(
+            universe_name=universe_name,
+            csv_file=os.path.join(sector_folder, f)
+        )
+
+        # Running backtest
+        backtest_results = reIndexer.Backtest(
+            sector_universe=candidate_universe
+        ).run()
+
+        # Saving output data to excel and pickle
+        backtest_results.to_excel(os.path.join(
+            excel_out_folder,
+            '.'.join([universe_name, 'xlsx'])
+        ))
+        backtest_results.to_pickle(os.path.join(
+            pickle_out_folder,
+            '.'.join([universe_name, '.pickle'])
+        ))
+
+        print('Completed backtesting {0}'.format(f))
+        counter += 1 # Increment counter (for percentage)
+        print('Backtest (all) {0}% complete'.format(counter /
+            len(candidate_files) * 100))
+        print('Completed {0} candidate universes; {1} remaining out of {2}'.\
+            format(counter, len(candidate_files) - counter,
+                   len(candidate_files)))
+
+if __name__ == '__main__':
+    backtestAll()
