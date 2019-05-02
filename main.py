a025248f0f3c3352a76b87af0bc6ed9493e2dfe7
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-05-02 15:36:00 -0400

    Updated main script to test S&P 500. Added sector file for SP500, and added output of the analysis to the correct places

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from reIndexer import Backtest, Universe
 import logging
+import numpy as np
 import pandas as pd
 
 # Setting log level
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 # Importing sector universe
 sp500 = Universe(
     universe_name='SP500',
-    csv_file='sector_universes/load_format/sp500.csv'
+    csv_file='sector_universes/learned_sector_candidates/sp500_final.csv'
 )
 
 # Runnizng backtest
 sim_results = Backtest(sector_universe=sp500).run()
 print(sp500.invalid_tickers)
-# Saving to csv
-sim_results.to_csv('tmp/out2.csv')
+
+# Saving to excel and pickle
+sim_results.to_excel('sector_universes/learned_sectors/excel/sp500.xlsx')
+sim_results.to_pickle('sector_universes/learned_sectors/pickle/sp500..pickle')
+
